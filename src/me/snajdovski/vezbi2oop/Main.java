ec416efee7c4b76ce87734d39e89e4a2d7f6e2c5
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-11-03 17:19:17 +0100

    init commit Student Class an Object of the student class

--- src/me/snajdovski/vezbi2oop/Main.java
+++ src/me/snajdovski/vezbi2oop/Main.java
@@ -7,5 +7,10 @@ public class Main {
     s1.surname = "Najdovski";
     s1.studentID = 123456;
     System.out.println(s1.name + " " + s1.surname + " " + s1.studentID);
+
+    Student s2 = new Student("Stefan", "Najdovski", 123456);
+    System.out.println(s2.name + " " + s2.surname + " " + s2.studentID);
+
+
     }
 }
